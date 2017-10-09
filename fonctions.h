d8b22d8698244e0cabe3416a966c48976f5ecb45
Edonadei <emrick.donadei@hotmail.fr>
2017-10-09 18:18:23 +0200

    Ajout des fonctions de base
    Ajout de la structure d'arbre n-aire

---
+++ fonctions.h
@@ -0,0 +1,16 @@
+#ifndef FONCTIONS_H_INCLUDED
+#define FONCTIONS_H_INCLUDED
+
+typedef struct arbre arbre;
+typedef struct arbre
+    {
+
+    char *namefile;
+    arbre *premierenfant;
+    arbre *suivant;
+
+    }arbre;
+
+
+
+#endif // FONCTIONS_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fonctions.h"
+
+int main()
+{
+    arbre explo = malloc(sizeof(arbre));
+    //init_arbre()
+    printf("Hello world!\n");
+    return 0;
+}
