65afd7db6b351661b54c0a112767b0991e5a22c8
DClaeyman <damien.claeyman.20160288@efrei.net>
2017-10-23 16:13:30 +0200

    Brouillon
    Pour l'instant il y a trois structures mais je ne sais pas si elles sont 100% fonctionelle. Je vais essayer de revoir tout ça.

--- main.c
+++ main.c
@@ -1,11 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "fonctions.h"
+
+#define MAX 256
+#define TAG_X 3
+#define TAG_Y 256
+
+
+
+/*typedef struct folder
+{
+    int info;
+    struct folder *next;
+}folder;
+
+typedef folder *listFolder;
+
+typedef struct file
+{
+    char name[MAX];
+    char tag[TAG_X][TAG_Y];
+    struct file *next;
+}file;
+
+typedef file *listFile;
+
+typedef struct node
+{
+    char name[MAX];
+    char tag[TAG_X][TAG_Y];
+    listFolder *son;
+    listFile *next;
+
+
+}node;
+
+typedef node *tree;*/
+
+typedef struct file
+{
+    char name[MAX];
+    char tag[TAG_X][TAG_Y];
+    struct file *next;
+}file;
+
+typedef file *listFile;
+
+typedef struct node
+{
+    char name[MAX];
+    char tag[TAG_X][TAG_Y];
+    struct node *son;
+    listFile *next;
+
+
+}node;
+
+typedef node *tree;
+
+int treeEmpty(tree t)
+{
+    if (t==NULL) return 0; //Vide
+    else return 1; //Pas vide
+
+}
+
+tree createTree(tree t)
+{
+    t=malloc(sizeof(node));
+    printf("Nom de l'arbre: ");
+    scanf("%s", t->name);
+    int i;
+    for (i=0;i<TAG_X;i++)
+    {
+        printf("Choisir TAG %d (en minuscule): ",i+1);
+        scanf("%s", t->tag[i]);
+    }
+    t->son=NULL;
+    t->next=NULL;
+
+    return t;
+}
+
+void printTree(tree t)
+{
+    if(t==NULL)
+    {
+        return;
+    }
+    printf("%s\n", t->name);
+    printTree(t->son);
+
+}
+
+void createNode(tree *t)
+{
+    tree a = (*t);
+
+    while(a->son != NULL)
+    {
+        a=a->son;
+        printf("caca");
+    }
+    a=malloc(sizeof(node));
+    printf("Nom de l'arbre: ");
+    scanf("%s", a->name);
+
+}
 
 int main()
 {
-    arbre explo = malloc(sizeof(arbre));
-    //init_arbre()
-    printf("Hello world!\n");
+    tree t=NULL;
+
+    t=createTree(t);
+    printf("%d\n", treeEmpty(t));
+    printTree(t);
+    createNode(&t);
+    createNode(&t);
+    printTree(t);
+
+
+
     return 0;
 }
